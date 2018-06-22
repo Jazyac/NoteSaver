a53db3b9e43c1d2465b768d145beb5e616391dc0
jazyac <jazy_ac@hotmail.com>
2018-06-23 00:24:40 +1200

    Design improvements on index

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -10,28 +10,21 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 <a class="navbar-brand" href="{{ URL::to('notes') }}">Notes.</a>
-
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="{{ URL::to('notes/create') }}">Start a Note.. <span class="sr-only">(current)</span></a>
-      </li>
-
-    </ul>
-  </div>
 </nav>
 </div>
 
 <!-- auto generetated by auth start-->
+@if (session('status'))
                     <div class="card-body">
-                        @if (session('status'))
+                 
                             <div class="alert alert-success" role="alert">
                                 {{ session('status') }}
                             </div>
-                        @endif
+                     
 
                         
                     </div>
+                    @endif
 <!-- auto generetated by auth finish-->
 
 @if($errors->any())
--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -2,26 +2,20 @@
 
 @section('notePageContent')
 
-
-
+<center>
+   <a class="btn btn-success btn-block" href="{{ URL::to('notes/create') }}">Start a Note.. </a>
+</center>
 <table class="table table-striped table-bordered">
-    <thead>
-        <tr>
-     
-            <td>Name</td>
-            <td>Content</td>
-     
-
-        </tr>
-    </thead>
+    
     <tbody>
     @foreach($notes as $key => $value)
         <tr>
          
             <td>{{ $value->name }}</td>
-            <td>{{ $value->content }}<br><a href="{{ route('notes.edit',$value->id) }}" class="btn btn-primary">Continue...</a>
+            <td>{{ str_limit($value->content, $limit = 100, $end = '...') }} </td>
             
-           
+            <td><a href="{{ route('notes.edit',$value->id) }}" class="btn btn-primary">Continue...</a> </td>
+            <td>
         {!! Form::open([
             'method' => 'DELETE',
             'route' => ['notes.destroy', $value->id]
@@ -35,12 +29,11 @@
           
             
   
-
-        
-
-            </td>
         </tr>
     @endforeach
+    <tr>
+ 
+    </tr>
     </tbody>
 </table>
 
