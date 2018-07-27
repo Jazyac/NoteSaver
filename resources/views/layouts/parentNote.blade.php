6f26aa912ba314457faa3e54497490e88d5dceb1
jazyac <jazy_ac@hotmail.com>
2018-07-27 21:21:35 +1200

    button look on note pages[D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[D[Ds[D[2~ks on note pages

--- resources/views/layouts/parentNote.blade.php
+++ resources/views/layouts/parentNote.blade.php
@@ -4,13 +4,21 @@
 
 <div class="container">
 
-<a style="float: left;" href="{{ URL::to('notes') }}"><span class="glyphicon glyphicon-arrow-left"></span></a>
+<a style="float: left;" href="{{ URL::to('notes') }}"><span class="btn btn-outline-primary glyphicon glyphicon-arrow-left"></span></a>
+
+
 
 
 @yield('notePageContent')
 
- {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+{!!
+    Form::macro('saveIconButton', function()
+{
+    return '<button style="float: right;" type="submit" value="Save" class="btn btn-outline-primary glyphicon glyphicon-floppy-disk">';
+});
+!!}
 
+    {!! Form::saveIconButton()!!}
 
 {!! Form::close() !!}
 
